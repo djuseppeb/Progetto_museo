d0c56d9eea4181cef1d63c6be46d8fe7e7389645
PeppeCasciola <75672913+PeppeCasciola@users.noreply.github.com>
2020-12-09 12:49:46 +0100

    Add files via upload

---
+++ Gui_Test.java
@@ -0,0 +1,64 @@
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Gui_Test {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	/*
+	public void testGet_Temperature() throws SQLException {
+		String temp = Prova_Gui.Get_Temperature()[2];
+		double x = Double.parseDouble(temp);
+		assertEquals(15.7,x,0.05); //Valore aspettato,Valore ottenuto,Soglia di errore
+	}
+	*/
+	/*
+	public void testGet_NumeroPersone() throws SQLException {
+		String temp = Prova_Gui.Get_NumeroPersone()[1];
+		double x = Double.parseDouble(temp);
+		assertEquals(9,x,0.05);
+	}
+	*/
+	/*
+	public void testGet_DistanzaMedia() throws SQLException {
+		String temp = Prova_Gui.Get_DistanzaMedia()[0];
+		double x = Double.parseDouble(temp);
+		assert(x>1);
+	}
+	*/
+	/*
+	public void testGet_Distanziamento() throws SQLException {
+		String temp = Prova_Gui.Get_Distanziamento()[0];
+		assert(temp.equals("rispettato"));
+	}
+	*/
+	/*
+	public void testGet_IntensitaLuci() throws SQLException {
+		String temp = Prova_Gui.Get_IntensitaLuci()[0];
+		int x = Integer.parseInt(temp);
+		assertEquals(6,x);
+	}
+	*/
+	/*
+	public void testGet_ImpiantoTermico() throws SQLException {
+		String temp = Prova_Gui.Get_ImpiantoTermico()[2];
+		assert(temp.equals("acceso"));
+	}
+	*/
+	
+	
+	
+
+}
