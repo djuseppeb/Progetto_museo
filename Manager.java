8f33f0165610b55263e8c77a26e8c7e7430350dc
Giuseppe Basile <48164486+NightsEdge97@users.noreply.github.com>
2020-12-14 16:25:58 +0100

    Add files via upload
    Aggiunto metodo getRoomList.
    Sistemati errori di battitura

--- Manager.java
+++ Manager.java
@@ -5,23 +5,31 @@ import java.util.List;
 
 public class Manager {
 	
-	public static List<Opera> getList(){
+	//Questo metodo restituisce una lista di oggetti di tipo Opera
+	public static List<Opera> getArtList(){
 		ArrayList<Opera> lista = new ArrayList<>();
 		int i = 1;
 		Creator getter = new OperaCreator();
 		Opera opera = (Opera) getter.createComponent(i);
-		
 		while(opera != null){
-	
 			lista.add(opera);
 			i++;
-			if (i>20)
-				break;
 			opera = (Opera) getter.createComponent(i);
-			
-		};
-		
-		
+		}
+		return lista;
+	}
+	
+	//Questo metodo restituisce una lista di oggetti di tipo Stanza
+	public static List<Room> getRoomList(){
+		ArrayList<Room> lista = new ArrayList<>();
+		int i = 1;
+		Creator getter = new RoomCreator();
+		Room room = (Room) getter.createComponent(i);
+		while(room != null){
+			lista.add(room);
+			i++;
+			room = (Room) getter.createComponent(i);
+		}
 		return lista;
 	}
 }
