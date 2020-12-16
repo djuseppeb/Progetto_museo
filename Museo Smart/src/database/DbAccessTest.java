1683cef28dee324c9bc903098750007822c26545
TanuzzuUMutu <75616476+TanuzzuUMutu@users.noreply.github.com>
2020-12-16 10:36:32 +0100

    Add files via upload
    Aggiunte nuove funzionalità e fixato qualche piccolo errore

---
+++ Museo Smart/src/database/DbAccessTest.java
@@ -0,0 +1,48 @@
+package database;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.sql.Connection;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DbAccessTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testGetAccess() {
+		DbAccess result = DbAccess.getAccess();
+		assertNotNull(result);
+	}
+	
+	@Test
+	void testConnect() {
+		Connection result = DbAccess.connect("gestione_museo");
+		assertNotNull(result);
+	}
+	
+	@Test
+    public void connect_shouldThrowException() {
+
+        try {
+
+            // This method is expected to throw a FooException
+        	Connection result = DbAccess.connect("blablabla");
+            
+            // If the exception is not thrown, the test will fail
+            fail("Expected exception has not been thrown");
+
+        } catch (Exception e) {
+
+            assertEquals(e.getMessage(), ("** DATABASE CONNECTION FAILED **"));
+        }
+    }
+}
---
+++ Museo Smart/src/database/RoomCreator.java
@@ -0,0 +1,40 @@
+package database;
+/*
+ * Class RoomCreator use factory pattern and is a concrete product creator that allows to create Room Object
+ * @author Albanese and Virruso
+ * @version 1.0
+ *
+ */
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class RoomCreator implements Creator{
+    Connection conn = DbAccess.getAccess().conn; //get Db istance
+ 
+    
+	@Override
+	public Component createComponent(int ID) {	//search room using ID
+		   Component room = null;
+		try {
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM room where ID_room = " +  ID );
+	        if(rs.next()) {						//if rs is not empty create room
+	        	room = new Room(rs.getInt("ID_room"), 
+	        			rs.getFloat("Temperature"), 
+	        			rs.getBoolean("Status_thermal_system"), 
+	        			rs.getInt("N_people"), 
+	        			rs.getFloat("Average_distance"), 
+	        			rs.getBoolean("Social_distancing"), 
+	        			rs.getInt("lights_intensity"));
+	        }
+		}
+		catch (Exception e) {
+			System.err.println("DB ERROR!");
+			System.err.println(e.getMessage());
+		}
+		
+		return room;
+	}	
+}
+	
