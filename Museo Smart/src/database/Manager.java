b8fce9c136b95f1c9c48016e9da48ad4461aca72
nerfedanto <75617539+nerfedanto@users.noreply.github.com>
2021-01-07 18:30:14 +0100

    Aggiunto metodo getNumOfVisitors() a Manager.java

--- Museo Smart/src/database/Manager.java
+++ Museo Smart/src/database/Manager.java
@@ -139,4 +139,14 @@ public class Manager {
 		
 		return list;
 	}
+	
+	//Returns the amount of people in the whole museum
+	public int getNumOfVisitors() 
+	{
+		int np = 0; //count people number
+		Manager manager = new Manager();
+		List<Room> rooms = manager.getRoomList();
+		for(int i = 0; i < rooms.size(); i++)	np = np + rooms.get(i).getPeople();
+		return np;
+	}
 }
