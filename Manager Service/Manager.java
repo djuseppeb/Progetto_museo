203da9412a3f5f727367bf1a1341ad2bcb3c1fbe
TanuzzuUMutu <75616476+TanuzzuUMutu@users.noreply.github.com>
2021-01-08 15:55:07 +0100

    New Manager Service Class
    Add this Classes in your springboot project. Please when you add function add also junit test function. If you don't, I will cry.

---
+++ Manager Service/Manager.java
@@ -0,0 +1,152 @@
+package database;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+@Service
+public class Manager {
+	
+	//Returns ArrayList with Opera objects
+	public  List<Opera> getArtList(){
+		ArrayList<Opera> lista = new ArrayList<>();
+		int i = 1;
+		Creator getter = new OperaCreator();
+		Opera opera = (Opera) getter.createComponent(i);
+		while(opera != null){
+			lista.add(opera);
+			i++;
+			opera = (Opera) getter.createComponent(i);
+		}
+		return lista;
+	}
+	
+	//Returns ArrayList with Room objects
+	public  List<Room> getRoomList(){
+		ArrayList<Room> lista = new ArrayList<>();
+		int i = 1;
+		Creator getter = new RoomCreator();
+		Room room = (Room) getter.createComponent(i);
+		while(room != null){
+			lista.add(room);
+			i++;
+			room = (Room) getter.createComponent(i);
+		}
+		return lista;
+	}
+
+	//Returns String with room info
+	public  String getRoomString(Room room){
+		String roomInfo = "";
+		String termalStatus ="";
+		if (room.getTermal())
+			termalStatus ="Acceso";
+			else
+				termalStatus ="Spento";
+		
+		String socialDistance = "";
+		if(room.isDistanceRespected())
+			socialDistance ="Rispettato";
+			else
+				socialDistance ="Non rispettato!";
+		
+		
+		roomInfo = "Temperatura: "+room.getTemp()+
+				"\nStato Impianto termico: "+termalStatus+
+				"\nNumero Persone: "+room.getPeople()+
+				"\nDistanziamento sociale "+socialDistance+
+				"\nStato luci: "+room.getLights();
+		
+		return roomInfo;
+	}
+
+	//Returns Art pieces available in a room
+	public  String isAvilableOpera(Opera art, int room){
+		if((art.getStatus().equals("Disponibile"))&&(art.getRoom()==room)){
+			return "\n>"+art.getTitle();
+		} else
+			return "";
+	}
+	
+	//Returns a String with all Art piece titles in a room
+	public  String getAvilableOperaString(ArrayList<Opera> listaOpere, int room){
+		String listAvailable = "";
+		for(int i = 0; i<listaOpere.size(); i++)
+			listAvailable += isAvilableOpera(listaOpere.get(i), room);
+		return listAvailable;
+	}
+
+	//Return a String with ID, Title, Artist and availability
+	public  String getStringOpera(Opera opera){
+		String string="";
+		string ="ID: "+opera.getID()+" - "+ opera.getTitle()+" - "+opera.getArtist()+" - "+opera.getStatus();
+		return string;
+	}
+	
+	//Return true if an Artwork ID exists
+	public  boolean operaExists(int ID) {
+		List<Opera> listaOpere = Manager.getArtList();
+		boolean exists = false;
+		for (Opera op : listaOpere) {
+			if (op.getID() == ID) {
+				exists = true;
+				break;
+			}
+		}
+		return exists;
+	}
+	
+	//Return an Opera object by id
+	public  Opera getOperaByID(int ID) {
+		List<Opera> listaOpere = Manager.getArtList();
+		for (Opera op : listaOpere) {
+			if (op.getID() == ID)
+				return op;
+		}
+		return null;
+	}
+	
+	//Return the incoming artwork list
+	public  List<Opera> getIncomingOpera() {
+		List<Opera> listaOpere = Manager.getArtList();
+		List<Opera> incomingArtworks = new ArrayList<>();
+		for (Opera op : listaOpere) {
+			if (op.getStatus().equals("In arrivo"))
+				incomingArtworks.add(op);
+		}
+		return incomingArtworks;
+	}
+	
+	//Returns the Artwork List but in alphabetic order, positive values means ascending order, negative values means descending order, 0 means default order: ascending order
+	public  List<Opera> getSortedArtList(int order) 
+	{	
+		List<Opera> list = Manager.getArtList(); //Creating art list
+        
+		Comparator<Opera> compareByTitle = (Opera operaX, Opera operaY) -> operaX.getTitle().compareTo( operaY.getTitle() ); //Creating Custom Comparator
+		
+		if(order >= 1)
+		{
+			Collections.sort(list, compareByTitle); //Ascending order
+		}
+		else if(order <= -1)
+		{
+			Collections.sort(list, compareByTitle.reversed() ); //Descending order
+		}
+		else
+		{
+			Collections.sort(list, compareByTitle); //Default order: Ascending order
+		}
+		
+		return list;
+	}
+	
+	//Returns the amount of people in the whole museum
+	public int getNumOfVisitors() 
+	{
+		int np = 0; //count people number
+		Manager manager = new Manager();
+		List<Room> rooms = manager.getRoomList();
+		for(int i = 0; i < rooms.size(); i++)	np = np + rooms.get(i).getPeople();
+		return np;
+	}
+}
---
+++ Manager Service/ManagerTest.java
@@ -0,0 +1,78 @@
+package database;
+
+import static org.junit.Assert.*;
+
+import java.util.*;
+
+import org.junit.Test;
+
+public class ManagerTest {
+	Manager manager = new Manager();
+	@Test
+	public void getRoomStringTest(){
+		Creator getter = new RoomCreator();
+		Room room = (Room) getter.createComponent(1);
+		String roomInfo = manager.getRoomString(room);
+		//fail();
+		assertEquals(roomInfo, "Temperatura: 16.3\nStato Impianto termico: Acceso\nNumero Persone: 7\nDistanziamento sociale Rispettato\nStato luci: 6");
+	}
+	
+	@Test
+	public void getAvilableOperaListTest(){
+		ArrayList<Opera> listaOpere = new ArrayList<>();
+		listaOpere = (ArrayList<Opera>) manager.getArtList();
+		
+		String operaList = manager.getAvilableOperaString(listaOpere, 1);
+		assertEquals (operaList, "\n>Gioconda\n>David");
+	}
+	
+	@Test
+	public void getStringOperaTest(){
+		Creator getter = new OperaCreator();
+		Opera opera = (Opera) getter.createComponent(2);
+		String operaString = manager.getStringOpera(opera);
+		assertEquals(operaString, "ID: 2 - Ultima Cena - Leonardo Da Vinci - In restauro");
+	}
+	
+	@Test
+	public void getSortedArtListTest()
+	{
+		int checker = 0; 
+		
+		List<Opera> listA = manager.getSortedArtList(1); //Ascending order (by title) art list
+		
+		for(Opera opera : listA)
+		{
+            if(opera.getID() == 6) //Checking if the first artpiece in the sorted list has ID 6 (Arrangiamento in grigio e nero, ritratto n. 1)
+            {
+            	checker++;
+            }
+            
+            break; //No need to check anything else in the sorted list
+        }
+		
+		List<Opera> listB = manager.getSortedArtList(-1); //Descending order (by title) art list
+		
+		for(Opera opera : listB) 
+		{
+            if(opera.getID() == 2) //Checking if the first artpiece in the sorted list has ID 2 (Ultima Cena)
+            {
+            	checker++;
+            }
+            
+            break; //No need to check anything else in the sorted list
+        }
+		
+		assertEquals(2, checker);
+	}
+	@Test
+	public void getNumOfVisitorsTest() {
+		Integer n1 = 0;
+		Integer	n2 = 0;
+		List<Room> rooms = manager.getRoomList();
+		for(int i = 0; i < rooms.size(); i++)	n1 = n1 + rooms.get(i).getPeople();
+		n2 = manager.numOfPeople();
+		assertNotNull(n1);
+		assertEquals(n1, n2);
+		}
+}
