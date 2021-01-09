f292449c714ac8bae3a648b3f656d98d036aee95
gaetano-1 <62390408+gaetano-1@users.noreply.github.com>
2021-01-09 16:39:39 +0100

    Add files via upload

---
+++ SmartMuseumWApp/src/main/java/it/gruppo5/smartmuseumwapp/SmartMuseumWAppApplication.java
@@ -0,0 +1,13 @@
+package it.gruppo5.smartmuseumwapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMuseumWAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartMuseumWAppApplication.class, args);
+	}
+
+}
---
+++ SmartMuseumWApp/src/test/java/it/gruppo5/smartmuseumwapp/SmartMuseumWAppApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gruppo5.smartmuseumwapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartMuseumWAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
